d0457d4e35edb045749434be0f382e771d0966ac
Eetu Häivälä <eetu.haivala@anders.fi>
2017-08-31 09:24:48 +0300

    Update README
    Refs RND-37

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     ),
     include_package_data=True,
     license='MIT',
-    long_description=open('README.md').read(),
+    long_description=open('README.rst').read(),
     description='Google Maps embed plugin for Django CMS',
     install_requires=[
         'django-cms>=3.2,<3.5',
